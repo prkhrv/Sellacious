56098e46a7ad0f8391f202cdb22f65a814282c8b
Piyush Mishra <piyushmishra1999@rediffmail.com>
2019-06-03 17:20:46 +0530

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,162 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-void main() => runApp(MyApp());
+import 'package:qr_reader/qr_reader.dart';
+
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return new MaterialApp(
+      title: 'Sellacious',
+      home: new MyHomePage(),
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.lightGreen
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
+
   final String title;
 
+  final Map<String, dynamic> pluginParameters = {
+  };
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future<String> _barcodeString;
+  TextEditingController roomController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    roomController = new TextEditingController(text:"https://");
+  }
 
   @override
   Widget build(BuildContext context) {
-    return WebviewScaffold(appBar: AppBar(
-      title: Text("Sellacious Demo"),
-      actions: <Widget>[
-        PopupMenuButton<String>(itemBuilder: null)
-      ],
-    ),
-        url: "https://google.com",
+    return new Scaffold(
+
+      appBar: new AppBar(
+        title: const Text('Sellacious'),
+      ),
+
+      body: new Center(
+          child: Stack(
+            children: <Widget>[
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+
+            Form(
+            child: Theme(
+      data: new ThemeData(
+          primaryColor: Colors.black,
+          brightness: Brightness.light,
+          primarySwatch: Colors.lightGreen,
+          inputDecorationTheme: new InputDecorationTheme(
+              labelStyle: new TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0
+              )
+          )
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(40.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            FlutterLogo(
+              size: 100.0,
+            ),
+            Padding(
+              padding:const EdgeInsets.symmetric(vertical: 16.0)
+            ),
+            new TextFormField(
+              controller: roomController,
+              decoration: new InputDecoration(
+                contentPadding: EdgeInsets.all(15.0),
+                border: new OutlineInputBorder(
+                    borderSide: new BorderSide(color: Colors.black,width: 5.0)
+                ),
+                labelText: "Enter the Url",
+                labelStyle: new TextStyle(fontFamily: 'Lato'),
+              ),
+              keyboardType: TextInputType.text,
+              cursorColor: Colors.black,
+            ),
+            new Padding(
+                padding: const EdgeInsets.only(top: 15.0 )
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Padding(padding: EdgeInsets.only(left: 150.0)),
+                new MaterialButton(
+                  height: 50.0,
+                  minWidth: 20.0,
+                  color: Colors.lightGreen,
+                  textColor: Colors.white,
+                  child: new Text(
+                    "Verify",
+                    style: new TextStyle(fontFamily: 'Lato'),
+
+                  ),
+                  onPressed: (){
+
+                  },
+                  splashColor: Colors.green,
+                )
+              ]
+            ),
+          new Padding(
+                padding: const EdgeInsets.only(top: 20.0)
+            ),
+            Text("Or Scan The QR code",
+              style: new TextStyle(fontSize: 20.0,color: Colors.black,fontWeight: FontWeight.bold,fontFamily:'Lato'),
+            ),
+            new Padding(
+                padding: const EdgeInsets.only(top: 20.0)
+            ),
+          new FloatingActionButton(
+                      onPressed: () {
+                        setState(() {
+                          _barcodeString = new QRCodeReader()
+                              .setAutoFocusIntervalInMs(200)
+                              .setForceAutoFocus(true)
+                              .setTorchEnabled(true)
+                              .setHandlePermissions(true)
+                              .setExecuteAfterPermissionGranted(true)
+                              .scan();
+                        });
+                      },
+                      tooltip: 'Reader the QRCode',
+                      child: new Icon(Icons.add_a_photo),
+                    ),
+          ]
+        )
+      )
+            )
+          ),
+
+            ],
+          ),
+
+            ],
+          )
+          ),
+
     );
   }
-
 }
